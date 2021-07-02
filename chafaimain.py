6eda30f9df0351a48422a6358d9b673d282112d8
PimKeer <pim.keer@gmail.com>
2021-07-02 10:01:47 +0200

    Updated site percolation test and created main file for Chafai simulations.

--- chafaimain.py
+++ chafaimain.py
@@ -3,10 +3,24 @@ from cluster import *
 from matplotlib import pyplot as plt
 from numba import njit
 from clustertest import *
+import genGFFChafai as ggch
 
-Nk = 10000 # Amount of samples made for the average of gamma.
-Narr = np.array([64]) # N's to use.
-parr = np.array([0.55,0.56,0.57,0.58]) # Cuts we try
+def levelset(x, h):
+    """Gives an array of 0's and 1's, where the sites with a 1 are in the level set above h."""
+    return np.where(x >= h, 1, 0)
+
+def gamma(x, h, N):
+    """Computes the second moment of the cluster size for a given DGFF sample and threshold h."""
+    y = levelset(x, h)
+    z = cluster(y, N)
+    clusterarray = np.bincount(np.bincount(z)[:-1])
+    gamma = (clusterarray * np.arange(len(clusterarray),dtype='int64')**2).sum()
+    return gamma
+
+
+Nk = 10000# Amount of samples made for the average of gamma.
+Narr = np.array([11]) # N's to use.
+parr = np.array([0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13]) # Cuts we try
 Np = len(parr) # Amount of cuts h we try.
 R = [] # List containing arrays of computed R_N values, one array for each N
 E = [] # List containing arrays of uncertainties in the R_N values, one array for each N
@@ -19,26 +33,17 @@ E = [] # List containing arrays of uncertainties in the R_N values, one array fo
 #      np.array([0.26310007,0.23105276,0.23043899,0.26088746,0.25148759,0.24457272,0.29336611,0.26809933,0.2446679,0.26841714,0.25030237,0.24790672,0.32812166,0.23355936]),
 #      np.array([0.17847925,0.17843752,0.20990995,0.16623804,0.19310972,0.12460153,0.26448589,0.2816964,0.29852619,0.26902651,0.2577366,0.31365023,0.33875275,0.21697951])]
 
-@njit
-def siteperc(N,p):
-    """Generate a Bernoulli site percolation sample on an NxN lattice, with occupation probability p."""
-    # x = np.random.random(N**2)
-    # y = np.where(x<=p,1,0)
-    y = np.random.binomial(size=N**2,n=1,p=p)
-    return y
-
-def gamma(x):
-    """Computes the second moment of the cluster size for a given sample of 1's and 0's."""
-    clusterarray = np.bincount(np.bincount(x)[:-1])
-    #print(np.bincount(x))
-    #print(clusterarray)
-    gamma = (clusterarray * np.arange(len(clusterarray),dtype='int64')**2).sum()
-    return gamma
-
 for i in range(len(Narr)):
     N = Narr[i] # Set lattice size
     Rh = np.zeros(Np) # This array will contain the R_N values for each given p, for the current N
     Eh = np.zeros(Np) # This array will contain the uncertainties in the R_N values for each given p, for the current N
+
+    if N == 6:
+        sG1 = np.load('2_gff_sG_6.txt.npy')
+        sG2 = np.load('2_gff_sG_11.txt.npy')
+    elif N == 11:
+        sG1 = np.load('2_gff_sG_11.txt.npy')
+        sG2 = np.load('njit_gff_sG_21.txt.npy')
 
     for j in range(Np):
         p = parr[j] # Set occupation probability
@@ -48,11 +53,14 @@ for i in range(len(Narr)):
         g2 = 0 # Running average of y2arr
         k = 0 # Iteration counter
         while k < Nk:
-            x1 = siteperc(N,p) # Generate a Bernoulli site percolation sample (lattice size N)
-            x2 = siteperc(2*N,p) # Generate a Bernoulli site percolation sample (lattice size 2N)
+            x1 = ggch.genGFFChafai2(N, sG1).reshape((N - 1) ** 3) # Generate a Chafai DGFF sample (lattice size N)
+            x2 = ggch.genGFFChafai2(2 * N - 1, sG2).reshape((2 * N - 2) ** 3)  # Generate a Chafai DGFF sample (lattice size 2N)
 
-            y1 = gamma(cluster2D(x1,N)) # Compute the second moment for cluster size
-            y2 = gamma(cluster2D(x2,2*N)) # Compute the second moment for cluster size
+            h1 = np.quantile(x1, 1 - parr[j], interpolation='midpoint') # Determine the threshold height given p
+            h2 = np.quantile(x2, 1 - parr[j], interpolation='midpoint') # Determine the threshold height given p
+
+            y1 = gamma(x1, h1, N - 1) # Compute the second moment for cluster size
+            y2 = gamma(x2, h2, 2 * N - 2) # Compute the second moment for cluster size
 
             g1 += y1 # Add this moment to the running average
             g2 += y2 # Add this moment to the running average
@@ -60,7 +68,7 @@ for i in range(len(Narr)):
             y1arr[k] = y1 # Add this moment to the moment list
             y2arr[k] = y2 # Add this moment to the moment list
 
-            if np.mod(k,100) == 0:
+            if np.mod(k,200) == 0:
                 print("N = ", N, "p = ", parr[j], "k = ", k, "  :  ", g2/g1, y1, y2) # Print out the values as a progress update
             k += 1 # Next iteration
         Rh[j] = g2/g1 # Add latest running average to list of R_N
@@ -82,32 +90,30 @@ for i in range(len(Narr)):
     print(Rh)
     print(Eh)
 
-# Plot the results
+
+
+col = ['r', 'g', 'b', 'y', 'k']
+line = ['-', '--', '-.', ':', '-']
+
+SMALL_SIZE = 14
+MEDIUM_SIZE = 14
+BIG_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIG_SIZE)     # fontsize of the figure title
+
 for i in range(len(Narr)):
     qm = np.poly1d(np.polyfit(parr, R[i], 2))
     x = np.linspace(parr[0], parr[-1], 10000)
-    plt.plot(x,qm(x), linestyle='--')
-    plt.errorbar(parr,R[i],yerr=E[i],capsize=5,linestyle='None')
+    plt.plot(x,qm(x), linestyle=line[i], color=col[i], label="N = "+str(Narr[i]))
+    plt.errorbar(parr,R[i],yerr=E[i],capsize=5,linestyle='None',ecolor=col[i])
+    # plt.xticks([0.585,0.59,0.595,0.60])
     plt.xlabel("$p$")
     plt.ylabel("$R_N$")
+    plt.legend()
 plt.show()
-
-# x1 = Label(N,p).reshape(N**2)
-# for l in range(len(x1)):
-#     if x1[l] == 0.:
-#         x1[l] = int(N**2)
-#     else:
-#         x1[l] = int(x1[l] - 1)
-#
-# x2 = Label(2*N, p).reshape((2*N)**2)
-# for l in range(len(x2)):
-#     if x2[l] == 0.:
-#         x2[l] = int((2*N) ** 2)
-#     else:
-#         x2[l] = int(x2[l] - 1)
-
-# x1 = np.array(x1,dtype='int64')
-# x2 = np.array(x2,dtype='int64')
-
-# y1 = gamma(x1)
-# y2 = gamma(x2)
